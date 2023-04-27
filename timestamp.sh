c8b1b7671c17803ad47708094d07b7a1aa39df80
Jasmine-Garcia <jas.garcia305@gmail.com>
2023-04-27 07:53:53 +0000

    jk saw a typo

--- timestamp.sh
+++ timestamp.sh
@@ -20,16 +20,16 @@ echo $oclock
 echo "The current date is: $today"
 echo "The current time is: $oclock"
 
-#Creat file containing date
+# Create file containing date:
 cat /var/log/syslog > "$today"_syslog.txt
 
-# A question within the context of today’s lab assignment
+# A question within the context of today’s lab assignment:
 # When trying to put the time in the file name it didn't quite work. I tried a few variations but I guess what would be the correct way to make sure the time is included?
 
-# An observation about the lab assignment, or related ‘Ah-hah!’ moment
+# An observation about the lab assignment, or related ‘Ah-hah!’ moment:
 # When the file created but then I realized the time wasn't included.
 
-# How long you spent working on this assignment
+# How long you spent working on this assignment:
 # An hour and a half - I'm not the strongest when it comes to bash and I'm not sure if I completely met the criteria.
 
 # End
