9acec746bdc71ab00cd47ec6a93ccd2bac7a5f9d
Jasmine-Garcia <jas.garcia305@gmail.com>
2023-04-27 06:08:53 +0000

    add new

---
+++ timestamp.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Script: Ops 301 Class 02 Ops Challenge Solution
+# Author: Jasmine Garcia
+# Date of latest revision: 4/26/2023
+# Purpose: Create a bash script that: Copies /var/log/syslog to the current working directory and appends the current date and time to the filename
+
