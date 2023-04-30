d273c2deca01d39f480786d4705a2af93bd573db
Jasmine-Garcia <jas.garcia305@gmail.com>
2023-04-30 00:40:50 +0000

    add new chall

---
+++ filepermissions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Script: Ops 301 Class 03 Ops Challenge Solution
+# Author: Jasmine Garcia
+# Date of latest revision: 4/29/2023
+# Purpose: Create a bash script that alters file permissions of everything in a directory.
+# Create a bash script that: 
+
+# Prompts user for input directory path.
+# Prompts user for input permissions number (e.g. 777 to perform a chmod 777)
+# Navigates to the directory input by the user and changes all files inside it to the input setting.
+# Prints to the screen the directory contents and the new permissions settings of everything in the directory.
+
+# Main
