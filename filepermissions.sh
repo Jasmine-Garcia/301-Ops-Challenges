8f0abbe0e90de8958d2d7c204f3ae0d69279db14
Jasmine-Garcia <jas.garcia305@gmail.com>
2023-05-04 04:55:46 +0000

    able to ssh again

--- filepermissions.sh
+++ filepermissions.sh
@@ -12,3 +12,20 @@
 # Prints to the screen the directory contents and the new permissions settings of everything in the directory.
 
 # Main
+
+echo "Warning: Altering file permissions can result in malfunctions to your OS."
+sleep  2
+
+echo "Proceed? [yes/no]"
+read yes
+echo "Okay, please input the directory path."
+read directory
+echo "Great, proceeding to change permissions. Please wait..."
+sleep 2
+
+# cd testingdirectory
+# chmod 777 beans.txt
+
+echo "End, or maybe not"
+
+- will need to come back to this - im currently trying to figure out how to upload from vs code through my server since im out of space. i have been working on this through replit
