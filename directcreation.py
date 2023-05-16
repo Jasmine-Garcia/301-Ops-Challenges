3e3fc66925f32c20bbf592f8c443415cd5e0306b
Jasmine Garcia <123357057+Jasmine-Garcia@users.noreply.github.com>
2023-05-15 20:42:03 -0700

    Update directcreation.py

--- directcreation.py
+++ directcreation.py
@@ -1,1 +1,31 @@
+# Script: Ops 301 Class 07 Ops Challenge Solution
+# Author: Jasmine Garcia
+# Date of latest revision: 5/15/2023
+# Purpose: Create a Python script that generates all directories, sub-directories and files for a user-provided directory path.
 
+# Script must ask the user for a file path and read a user input string into a variable.
+# Script must use the os.walk() function from the os library.
+# Script must enclose the os.walk() function within a python function that hands it the user input file path.
+
+#!/usr/bin/env python3
+
+#Main
+
+# Import libraries
+
+import os
+
+# Declaration of variables
+
+user = input("What is the file path?")
+print("You have entered the file path: ivybean_backups", path)
+
+# Declaration of functions
+
+def ivybean_backups(path)
+  for (root, dirs, files) in os.walk("ivybean_backups")
+    print(==root==)
+    print(==dirs==)
+    print(==files==)
+
+# End
