8b2a3e5adc8bab35e7446f8adfa1eed2534b5e7f
Jasmine Garcia <123357057+Jasmine-Garcia@users.noreply.github.com>
2023-05-18 00:31:42 -0700

    final edit

--- pycollections.py
+++ pycollections.py
@@ -8,6 +8,24 @@
 # Print the sixth through tenth element of the list.
 # Change the value of the seventh element to “onion”.
 
-#Main
+# Main
 
-#!/usr/bin/env python3
+# !/usr/bin/env python3
+
+# List of ten string elements
+cute_dogs = ["corgi", "shepherd", "poodle", "husky", "shiba", "samoyed", "terrier", "labrador", "retriever", "dalmatian"] 
+
+# Print the fourth element of the list
+print("fourth element:", cute_dogs[3])
+
+# Print the sixth through tenth element of the list
+print("sixth through tenth elements:", cute_dogs[5:9])
+
+# Change the value of the seventh element to “onion”
+cute_dogs[6] = "onion"
+
+print("updated list:", cute_dogs)
+
+# End
+
+# References: Hexx's Challenge 8 Replit in class repo, ChatGBT - sceenshot is attached to comments
