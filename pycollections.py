555a9b9b17570bbc33587a5e66ae7f2883cb9786
Jasmine Garcia <123357057+Jasmine-Garcia@users.noreply.github.com>
2023-05-17 23:12:07 -0700

    Create pycollections.py

---
+++ pycollections.py
@@ -0,0 +1,13 @@
+# Script: Ops 301 Class 08 Ops Challenge Solution
+# Author: Jasmine Garcia
+# Date of latest revision: 5/17/2023
+# Create a Python script that includes the following:
+
+# Assign to a variable a list of ten string elements.
+# Print the fourth element of the list.
+# Print the sixth through tenth element of the list.
+# Change the value of the seventh element to “onion”.
+
+#Main
+
+#!/usr/bin/env python3
