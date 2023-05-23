320d392ad71e3b471a213e1d15f27081a66c057c
Jasmine Garcia <123357057+Jasmine-Garcia@users.noreply.github.com>
2023-05-22 18:28:01 -0700

    first commits

---
+++ conditionals.py
@@ -0,0 +1,41 @@
+# !/usr/bin/env python3
+
+# Script: Ops 301 Class 08 Ops Challenge Solution
+# Author: Jasmine Garcia
+# Date of latest revision: 5/22/2023
+# Create Create if statements using these logical conditionals below. Each statement should print information to the screen depending on if the condition is met.
+
+# Equals: a == b
+# Not Equals: a != b
+# Less than: a < b
+# Less than or equal to: a <= b
+# Greater than: a > b
+# Greater than or equal to: a >= b
+
+# Create an if statement using a logical conditional of your choice and include elif keyword that executes when other conditions are not met.
+# Create an if statement that includes both elif and else to execute when both if and elif are not met.
+
+# Numbers equal elevation gain
+
+# Variable Declarations 
+
+activity = hike
+elevation = 2000
+difficuly = "moderate"
+miles = 8
+forecast = "sunny"
+hours = 6
+
+# Main
+
+# if statement 
+if difficulty == "moderate":
+  print("It's a moderate hike, let's give this hike a try!")
+  
+# if-else statement 
+if forecast != sunny
+  print("This isn't the best hiking weather, let's try another day.")
+  
+  
+
+
