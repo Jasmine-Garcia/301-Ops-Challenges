3e89572d6cef987f67e07e0d8de04cde54bda912
Jasmine Garcia <123357057+Jasmine-Garcia@users.noreply.github.com>
2023-05-29 01:32:13 -0700

    made corrections part 2

--- psutil.py
+++ psutil.py
@@ -25,35 +25,35 @@ print("Time spent by normal processes executing in user mode:", normal_time)
 
 # Time spent by processes executing in kernel mode
 kernel_time = psutil.cpu_times().system
-print("Time spent by normal processes executing in user mode:", kernel_time)
+print("Time spent by normal processes executing in kernel mode:", kernel_time)
 
 # Time when system was idle
 idle_time = psutil.cpu_times().idle
-print("Time spent by normal processes executing in user mode:", idle_time)
+print("Time when system was idle:", idle_time)
 
 # Time spent by priority processes executing in user mode
 priority_time = psutil.cpu_times().nice
-print("Time spent by normal processes executing in user mode:", priority_time)
+print("Time spent by priority processes executing in user mode:", priority_time)
 
 # Time spent waiting for I/O to complete.
 IO_time = psutil.cpu_times().iowait
-print("Time spent by normal processes executing in user mode:", IO_time)
+print("Time spent waiting for I/0 to complete:", IO_time)
 
 # Time spent for servicing hardware interrupts
 servicing_hardware_time = psutil.cpu_times().irq
-print("Time spent by normal processes executing in user mode:", servicing_hardware_time)
+print("Time spent for servicing hardware interrupts:", servicing_hardware_time)
       
 # Time spent for servicing software interrupts
 servicing_software_time = psutil.cpu_times().softirq
-print("Time spent by normal processes executing in user mode:", servicing_software_time)
+print("Time spent sevicing software interrupts:", servicing_software_time)
       
 # Time spent by other operating systems running in a virtualized environment
 virtualization_time = psutil.cpu_times().steal
-print("Time spent by normal processes executing in user mode:", virtualization_time)
+print("Time spent by other operating systems running in a virtualized environment:", virtualization_time)
 
 # Time spent running a virtual CPU for guest operating systems under the control of the Linux kernel
 linux_time = psutil.cpu_times().guest
-print("Time spent by normal processes executing in user mode:", linux_time)
+print("Time spent running a virtual CPU for guest operating systems under the control of the Linux kernel:", linux_time)
 
 # Resources: GeeksforGeeks: Psutil Module
 
