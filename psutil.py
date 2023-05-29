f7ed0d2d748e43869fd3e5792d2a0e7046e8f0db
Jasmine Garcia <123357057+Jasmine-Garcia@users.noreply.github.com>
2023-05-29 01:27:37 -0700

    made corrections

--- psutil.py
+++ psutil.py
@@ -24,33 +24,33 @@ normal_time = psutil.cpu_times().user
 print("Time spent by normal processes executing in user mode:", normal_time)
 
 # Time spent by processes executing in kernel mode
-kernel_time = psutil.cpu_times().user
+kernel_time = psutil.cpu_times().system
 print("Time spent by normal processes executing in user mode:", kernel_time)
 
 # Time when system was idle
-idle_time = psutil.cpu_times().user
+idle_time = psutil.cpu_times().idle
 print("Time spent by normal processes executing in user mode:", idle_time)
 
 # Time spent by priority processes executing in user mode
-priority_time = psutil.cpu_times().user
+priority_time = psutil.cpu_times().nice
 print("Time spent by normal processes executing in user mode:", priority_time)
 
 # Time spent waiting for I/O to complete.
-IO_time = psutil.cpu_times().user
+IO_time = psutil.cpu_times().iowait
 print("Time spent by normal processes executing in user mode:", IO_time)
 
 # Time spent for servicing hardware interrupts
-servicing_hardware_time = psutil.cpu_times().user
+servicing_hardware_time = psutil.cpu_times().irq
 print("Time spent by normal processes executing in user mode:", servicing_hardware_time)
       
 # Time spent for servicing software interrupts
-servicing_software_time = psutil.cpu_times().user
+servicing_software_time = psutil.cpu_times().softirq
 print("Time spent by normal processes executing in user mode:", servicing_software_time)
       
 # Time spent by other operating systems running in a virtualized environment
-virtualization_time = psutil.cpu_times().user
+virtualization_time = psutil.cpu_times().steal
 print("Time spent by normal processes executing in user mode:", virtualization_time)
 
 # Time spent running a virtual CPU for guest operating systems under the control of the Linux kernel
-linux_time = psutil.cpu_times().user
+linux_time = psutil.cpu_times().guest
 print("Time spent by normal processes executing in user mode:", linux_time)
