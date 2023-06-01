1821e98bf4c2db41ccab04673351a8edcf324234
Jasmine Garcia <123357057+Jasmine-Garcia@users.noreply.github.com>
2023-05-31 23:41:24 -0700

    add new file

---
+++ requestslibrary.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Script: Ops 301 Challenge 12
+# Author: Jasmine Garcia
+# Date of last revision: 5/31/23
+# Purpose: Create a Python script that performs the following:
+# Prompt the user to type a string input as the variable for your destination URL.
+# Prompt the user to select a HTTP Method of the following options:
+
+  # GET
+  # POST
+  # PUT
+  # DELETE
+  # HEAD
+  # PATCH
+  # OPTIONS
+  
+# Print to the screen the entire request your script is about to send. Ask the user to confirm before proceeding.
+# Using the requests library, perform a GET request against your lab web server.
+# For the given header, translate the codes into plain terms that print to the screen; for example, a 404 error should print Site not found to the terminal instead of 404.
+# For the given URL, print response header information to the screen.
